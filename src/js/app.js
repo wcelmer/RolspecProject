7650663cff8a8184e0c6a1687bf888c77d259bf6
wcelmer <w.celmer@me.com>
2018-04-23 14:15:31 +0200

    szablon

---
+++ src/js/app.js
@@ -0,0 +1,70 @@
+// Google Maps
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+
+  $('#navbarSideButton').on('click', function() {
+    $('#navbarSide').addClass('reveal')
+  });
+
+  // Open navbarSide when button is clicked
+  $('#navbarSideButton').on('click', function() {
+    $('#navbarSide').addClass('reveal');
+    $('.overlay').show();
+  });
+
+  // Close navbarSide when the outside of menu is clicked
+  $('.overlay').on('click', function(){
+    $('#navbarSide').removeClass('reveal');
+    $('.overlay').hide();
+  });
+
+
+  // SCROLLING PAGE
+
+  $("#navbarSide a").click(function() {
+    $("html, body").animate({ scrollTop: $($(this).attr('href')).offset().top - 60 }, "slow");
+    return false;
+  });
+
+
+
+function initMap() {
+        var uluruNedzerzew = {lat: 52.1338361, lng: 19.3058513};
+        var uluruWitonia = {lat: 52.146949, lng: 19.294851};
+        var uluruGrabow = {lat: 52.1270918, lng: 19.0059362};
+        var uluruDaszyna = {lat: 52.155714, lng: 19.182794};
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 12,
+          center: uluruDaszyna
+        });
+        var markerNedz = new google.maps.Marker({
+          position: uluruNedzerzew,
+          map: map
+        });
+        var markerWit = new google.maps.Marker({
+          position: uluruWitonia,
+          map: map
+        });
+        var markerGrab = new google.maps.Marker({
+          position: uluruGrabow,
+          map: map
+        });
+        var markerDasz = new google.maps.Marker({
+          position: uluruDaszyna,
+          map: map
+        });
+      }
+
+  initMap();
+
+
+});
+
+
+
+
+
+
+//
